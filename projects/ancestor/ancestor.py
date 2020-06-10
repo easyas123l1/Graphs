adcc32a928237a92cb3c7455d9697b8aee14fcdc
easyas123l1 <andrew.wilson90@hotmail.com>
2020-06-10 16:35:09 -0500

    more changes

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -5,35 +5,55 @@ from collections import deque
 
 def earliest_ancestor(ancestors, starting_node):
     stack = deque()
+    # starting node as first location
     stack.append([starting_node])
+    # cache for all ancestors 
     cache = {}
+    # results is all last parent nodes
     results = []
+    # path is the destination it takes to get to the location
     path = []
+    # loop thru ancestors and store them in a cache of arrays for each locations possible ways.
     for ancestor in ancestors:
         if ancestor[1] in cache:
             cache[ancestor[1]].append(ancestor[0])
         else:
             cache[ancestor[1]] = [ancestor[0]]
+    # if our start goes no where then return -1 edge case
     if starting_node not in cache:
         return -1
+    # loop while we still have something in memory
     while len(stack) > 0:
         print(stack)
+        # pop off the last item to look for possible directions
         current_array = stack.pop()
-        while len(current_array) > 0:
+        if len(current_array) != 0:
+            # loop thru all possible directions
             current = current_array.pop()
             path.append(current)
             if current in cache:
+                stack.append(current_array)
                 stack.append(cache[current])
+                current_array = []
             else:
-                results.append((len(stack), current))
+                # add to results the len of path and the current node.
+                results.append((len(path), current))
+                # print(path)
+                # print(results)
+                # print(stack)
+                path.pop()
+        else:
+            path.pop()
+    
     best = results[0]
-    print(results)
+    # find the longest len and tie breaker by smallest number
     for result in results:
         if result[0] == best[0]:
             if result[1] < best[1]:
                 best = result
         if result[0] > best[0]:
             best = result
+    # return the node associated to the longest len
     return best[1]
     #     if ancestor[1] == current:
     #         current = ancestor[0]
