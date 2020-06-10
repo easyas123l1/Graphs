d5cdf96c7a2147ee9839123108f7f944249323bf
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-06-10 15:03:41 -0500

    after day 3 learning

---
+++ connectcomponents.py
@@ -0,0 +1,12 @@
+Connected Components
+--------------------
+
+Parts of the graph that are connected, but disjoint from other parts of the
+graph.
+
+To count connected componets:
+
+    For each node:
+        if node not visited:
+            traverse from that node
+            increment counter
